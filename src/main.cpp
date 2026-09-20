ce07726e24cbc3fbf93e16b0840f058be3d811eb
DanielSaavedraNavarro <danielsn.contacto@gmail.com>
2026-09-20 12:36:11 -0300

    Actualizar main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,15 @@
-// force code
+#include <Geode/Geode.hpp>
+#include <Geode/modify/CCDirector.hpp>
+
+using namespace geode::prelude;
+
+class $modify(Force9x16Director, CCDirector) {
+    void reshapeProjection(CCSize size) {
+        CCSize targetSize = CCSize(
+            size.height * 9.0f / 16.0f,
+            size.height
+        );
+
+        CCDirector::reshapeProjection(targetSize);
+    }
+};
